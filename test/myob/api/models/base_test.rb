59266e1f39642e0932ad15119fabe69cd5a7a0ac
David Lumley <davidlumley>
2015-03-23 19:43:02 +1000

    add test for all method of model

--- test/myob/api/models/base_test.rb
+++ test/myob/api/models/base_test.rb
@@ -3,7 +3,12 @@ require 'test_helper'
 class BaseTest < Minitest::Test
 
   def setup
+    @class = Myob::Api::Model::Base
     @model = Myob::Api::Model::Base.new({})
+  end
+
+  def test_all_method_returns_array
+    assert_kind_of Array, @class.all
   end
 
 end
