c44bd04c8bea53968a73ee226f1a22832f83be49
David Lumley <davidlumley>
2015-03-24 20:12:18 +1000

    add empty contact model

--- lib/myob-api.rb
+++ lib/myob-api.rb
@@ -4,5 +4,6 @@ require 'myob/api/helpers'
 require 'myob/api/query'
 
 require 'myob/api/models/base'
+require 'myob/api/models/contact'
 
 require 'myob/api/client'
---
+++ lib/myob/api/models/contact.rb
@@ -0,0 +1,8 @@
+module Myob
+  module Api
+    module Model
+      class Contact < Base
+      end
+    end
+  end
+end
