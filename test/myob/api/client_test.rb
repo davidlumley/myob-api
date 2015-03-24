1a6b6a5eeb3190bda0a47152f94e556727b65448
David Lumley <davidlumley>
2015-03-24 20:18:12 +1000

    add test to ensure client has model methods attached

--- test/myob/api/client_test.rb
+++ test/myob/api/client_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class ClientTest < Minitest::Test
 
+  @models = [
+    :contact,
+  ]
+
   def setup
     @model_class = MiniTest::Mock.new
     @class       = Myob::Api::Client
@@ -15,5 +19,11 @@ class ClientTest < Minitest::Test
     assert_respond_to @client, :test_model
     assert_kind_of Myob::Api::Query, @client.test_model
   end
-  
+
+  @models.each do |model_method|
+    define_method("test_client_responds_to_#{model_method}_method") do
+      assert_respond_to @client, model_method
+    end
+  end
+
 end
