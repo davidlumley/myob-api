ec40ba0b5eb77a7ffe72afc893e4884a8c3b68e2
David Lumley <davidlumley>
2015-03-24 20:20:59 +1000

    rename models dir to model dir

--- lib/myob-api.rb
+++ lib/myob-api.rb
@@ -3,7 +3,7 @@ require 'myob/api/version'
 require 'myob/api/helpers'
 require 'myob/api/query'
 
-require 'myob/api/models/base'
-require 'myob/api/models/contact'
+require 'myob/api/model/base'
+require 'myob/api/model/contact'
 
 require 'myob/api/client'
