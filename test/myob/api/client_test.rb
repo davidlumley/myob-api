fcc5b4fb4b8b4e3ecd577e1c7389c04b8edd0f30
David Lumley <davidlumley>
2015-03-24 19:38:59 +1000

    refactor test to create class in setup

--- test/myob/api/client_test.rb
+++ test/myob/api/client_test.rb
@@ -2,15 +2,16 @@ require 'test_helper'
 
 class ClientTest < Minitest::Test
 
-  class TestModel; end
-
   def setup
-    @class  = Myob::Api::Client
-    @client = Myob::Api::Client.new({})
+    @model_class = MiniTest::Mock.new
+    @class       = Myob::Api::Client
+    @client      = Myob::Api::Client.new({})
   end
 
   def test_define_model_method_creates_instance_method
-    @class.define_model_method(TestModel)
+    @model_class.expect(:name, 'TestModel')
+    @class.define_model_method(@model_class)
+    @model_class.verify
     assert_respond_to @client, :test_model
   end
 
