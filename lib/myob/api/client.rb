18e5e2a0ab484610ce7dd7c6989ac7cf4dc2438c
David Lumley <davidlumley>
2015-03-24 19:18:35 +1000

    add method to client to allow querying of models

--- lib/myob/api/client.rb
+++ lib/myob/api/client.rb
@@ -2,6 +2,17 @@ module Myob
   module Api
     class Client
 
+      extend Myob::Api::Helpers
+
+      class << self
+        def define_model_method(model_class)
+          model_name = model_class.name.split('::').last
+          define_method(underscore(model_name)) do
+            Myob::Api::Query.new(model_class)
+          end
+        end
+      end
+
       def initialize(options)
       end
 
--- test/myob/api/client_test.rb
+++ test/myob/api/client_test.rb
@@ -2,8 +2,16 @@ require 'test_helper'
 
 class ClientTest < Minitest::Test
 
+  class TestModel; end
+
   def setup
+    @class  = Myob::Api::Client
     @client = Myob::Api::Client.new({})
+  end
+
+  def test_define_model_method_creates_instance_method
+    @class.define_model_method(TestModel)
+    assert_respond_to @client, :test_model
   end
 
 end
