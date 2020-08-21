e7ad0011cc8d352ebc6882b1c0a968367427b6ea
Mitch Smith <mitchpsmith1998@gmail.com>
2020-08-21 14:37:41 +1000

    fix incorrect name for model

--- lib/myob/api/models/superannuation.rb
+++ lib/myob/api/models/superannuation.rb
@@ -1,7 +1,7 @@
 module Myob
   module Api
     module Model
-      class PayrollCategory < Base
+      class SuperannuationFund < Base
         def model_route
           '/Payroll/SuperannuationFund'
         end
