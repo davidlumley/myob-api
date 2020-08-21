c4efc876298997fa41fd6aa1c99a695a187b338e
Mitch Smith <mitchpsmith1998@gmail.com>
2020-08-21 14:35:40 +1000

    add superannuation endpoint

---
+++ lib/myob/api/models/superannuation.rb
@@ -0,0 +1,11 @@
+module Myob
+  module Api
+    module Model
+      class PayrollCategory < Base
+        def model_route
+          '/Payroll/SuperannuationFund'
+        end
+      end
+    end
+  end
+end
