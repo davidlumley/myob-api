177f585cee8e9af06b1d0a25f46a136732acd24f
Darwin Tantuco <dcrtantuco@gmail.com>
2021-01-04 16:33:55 +0800

    Adds support for invoice service, professional, time billing and miscellaneous types

---
+++ lib/myob/api/models/invoice_miscellaneous.rb
@@ -0,0 +1,11 @@
+module Myob
+  module Api
+    module Model
+      class InvoiceMiscellaneous < Base
+        def model_route
+          'Sale/Invoice/Miscellaneous'
+        end
+      end
+    end
+  end
+end
---
+++ lib/myob/api/models/invoice_time_billing.rb
@@ -0,0 +1,11 @@
+module Myob
+  module Api
+    module Model
+      class InvoiceTimeBilling < Base
+        def model_route
+          'Sale/Invoice/TimeBilling'
+        end
+      end
+    end
+  end
+end
