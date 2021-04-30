36ef778b63b1dcd144337870e473f7d1646c3148
Darwin Tantuco <dcrtantuco@gmail.com>
2021-04-30 10:01:41 +0800

    Support for /Report/BalanceSheetSummary

---
+++ lib/myob/api/models/balance_sheet_summary.rb
@@ -0,0 +1,11 @@
+module Myob
+  module Api
+    module Model
+      class BalanceSheetSummary < Base
+        def model_route
+          'Report/BalanceSheetSummary'
+        end
+      end
+    end
+  end
+end
